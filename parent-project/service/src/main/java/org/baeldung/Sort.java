441d8f59c99393efa1f203152f4fbd424daa5bb6
aslam <muhammadh.aslamm@gmail.com>
2021-04-05 16:17:44 +0530

    test comments removed

--- parent-project/service/src/main/java/org/baeldung/Sort.java
+++ parent-project/service/src/main/java/org/baeldung/Sort.java
@@ -9,13 +9,7 @@ import java.util.Arrays;
 public class Sort {
 
     public static void main( String[] args ) {
-<<<<<<< HEAD
-//from master
-//      added some text from b1
-=======
-// new b1
-//        added some text from b1
->>>>>>> b1
+
         int array[] = new int[args.length];
         getIntArray(args, array);
 
