53f4e99585eb8720d907544f5f8a6708cb75029b
Aslam <muhammadh.aslamm@gmail.com>
2021-04-05 11:45:05 +0530

    new: sorting algorithms

---
+++ parent-project/core/src/main/java/org/baeldung/BubbleSort.java
@@ -0,0 +1,28 @@
+package org.baeldung;
+
+// Java program for implementation of Bubble Sort
+public class BubbleSort
+{
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
+
+    /* Prints the array */
+    void printArray(int arr[])
+    {
+        System.out.print("Sorted Array: ");
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+}
---
+++ parent-project/service/src/main/java/org/baeldung/Sort.java
@@ -0,0 +1,73 @@
+package org.baeldung;
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sort {
+
+    public static void main( String[] args ) {
+        int array[] = new int[args.length];
+        getIntArray(args, array);
+
+        int[] arrayCopy1 = Arrays.copyOf(array, array.length);
+        int[] arrayCopy2 = Arrays.copyOf(array, array.length);
+
+        System.out.println("\n\n");
+        Sort sort = new Sort();
+        sort.runInsertionSort(array);
+        sort.runSelectionSort(arrayCopy1);
+        sort.runBubbleSort(arrayCopy2);
+        System.out.println("\n\n");
+    }
+
+    private static void getIntArray(String [] args, int [] array) {
+        try{
+            for (int i=0; i<args.length; i++) {
+                array[i] = Integer.parseInt(args[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("\nException occurred: " + e + "\n");
+            System.exit(0);
+        }
+    }
+
+    private void runInsertionSort(int arr[]){
+        InsertionSort insertionSort = new InsertionSort();
+
+        int n = arr.length;
+
+        long startTime = System.nanoTime();
+        insertionSort.sort(arr);
+        long endTime = System.nanoTime();
+
+        insertionSort.printArray(arr);
+        System.out.println("Time taken: "+(endTime - startTime) + " ns  (Insertion)");
+    }
+
+    private void runBubbleSort(int arr[]){
+        BubbleSort bubbleSort = new BubbleSort();
+
+        int n = arr.length;
+
+        long startTime = System.nanoTime();
+        bubbleSort.bubbleSort(arr);
+        long endTime = System.nanoTime();
+
+        bubbleSort.printArray(arr);
+        System.out.println("Time taken: "+(endTime - startTime) + " ns  (Bubble)");
+    }
+
+    private void runSelectionSort(int arr[]){
+        SelectionSort selectionSort = new SelectionSort();
+
+        long startTime = System.nanoTime();
+        selectionSort.sort(arr);
+        long endTime = System.nanoTime();
+
+        selectionSort.printArray(arr);
+        System.out.println("Time taken: "+(endTime - startTime) + " ns  (Selection)");
+    }
+}
