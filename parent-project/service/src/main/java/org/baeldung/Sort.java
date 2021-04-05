d2241ff6c05c7e2679da519fe6c7e2c23c1b2ad1
aslam <muhammadh.aslamm@gmail.com>
2021-04-05 16:16:06 +0530

    from master

--- parent-project/service/src/main/java/org/baeldung/Sort.java
+++ parent-project/service/src/main/java/org/baeldung/Sort.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public class Sort {
 
     public static void main( String[] args ) {
-
+//from master
 //      added some text from b1
         int array[] = new int[args.length];
         getIntArray(args, array);
